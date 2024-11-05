1c9373014e52393b823aed5a9504b4e901b7cf13
Landon Clarke <89565789+Larke20@users.noreply.github.com>
2024-11-05 11:04:00 -0600

    test case criteria met

--- src/test/java/qsortsTest.java
+++ src/test/java/qsortsTest.java
@@ -20,7 +20,7 @@ class qsortsTest {
         assertArrayEquals(expected, input);
     }
     @Test
-    // test case 3
+    // test case 3 // FALLS UNDER NEGATIVE CASES
     public void nullArr() {
         int[] input = null;
         assertThrows(NullPointerException.class, () -> {
@@ -28,10 +28,11 @@ class qsortsTest {
         });
     }
     static Stream<Arguments> generator(){
-        Arguments tc1 = Arguments.of("Array of 3", new int[]{3,1,2});
-        Arguments tc2 = Arguments.of("Empty Array", new int[]{});
-        Arguments tc4 = Arguments.of("Negative Array", new int[]{-4,-2,-3,-1});
-        Arguments tc5 = Arguments.of("Array of One", new int[]{0}); //
+        Arguments tc1 = Arguments.of("Array of 3", new int[]{3,1,2}); // FALLS UNDER POSITIVE CASES
+        Arguments tc2 = Arguments.of("Empty Array", new int[]{}); // FALLS UNDER BOUNDARY CASES
+        Arguments tc4 = Arguments.of("Large Array", new int[]{-4,-2,-3,-1, -10, -12, -5, 1, 5}); // FALLS UNDER PERFORMANCE CASES
+        Arguments tc5 = Arguments.of("Array of 3", new int[]{3,1,2}); // FALLS UNDER IDEMPOTENCY
+
         return Stream.of(tc1, tc2, tc4, tc5);
     }
   
